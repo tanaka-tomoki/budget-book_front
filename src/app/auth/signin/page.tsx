82431a9599aa327cca5cc111691c5d482fdc1df1
tomoki tanaka <t.tanaka@c4c.co.jp>
2025-04-21 23:06:51 +0900

    Typescriptでのプロジェクト作り直し

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,8 @@
+// ログインページ
+
+const Login = (() => {
+    return (
+        <div>ログイン画面</div>
+    )
+})
+export default Login;
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Login from './auth/signin/page';
+
+export default function Home() {
+  return (
+    <>
+     <Login />
+    </>
+  );
+}
