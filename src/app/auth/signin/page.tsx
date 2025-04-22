dfd9724eb92f0e6b1e211baa91c07cdfe205bf48
tomoki tanaka <t.tanaka@c4c.co.jp>
2025-04-23 00:25:07 +0900

    ログイン画面のレイアウト実装

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,8 +1,72 @@
-// ログインページ
+'use client';
 
+// ログインページ
+import { useState } from 'react';
+import { Button, TextField } from '@mui/material';
 const Login = (() => {
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Todo: ログイン処理を実装する
+
     return (
-        <div>ログイン画面</div>
+        <>
+            {/*タイトル*/}
+            <div className="bb-content title-content pt-36">
+                {/* タイトル名と画像 */}
+                <div className=''>
+                    <p>
+                        <span className="font-bold text-4xl text-black">Budget</span>
+                        <span className="font-bold text-4xl text-[#FF8C00]">Book</span>
+                    </p>
+                    <img src={'/logo.png'} />
+                </div>
+                {/* 入力エリア */}
+                <div className='pt-10 flex flex-col'>
+                    <TextField
+                        label="ユーザID"
+                        variant='filled'
+                        margin='normal'
+                        sx={{
+                            width: '245px',
+                            height: '53px',
+                            background: '#ffffff',
+                            boxShadow: '2px 5px 5px gray',
+                            borderRadius: '5px'
+                        }}
+                     />
+                     <TextField
+                        label="パスワード"
+                        variant='filled'
+                        margin='normal'
+                        sx={{
+                            width: '245px',
+                            height: '53px',
+                            background: '#ffffff',
+                            boxShadow: '2px 5px 5px gray',
+                            borderRadius: '5px'
+                        }}
+                     />
+                </div>
+                {/* ボタンエリア */}
+                <div className='pt-10 flex flex-col'>
+                    <Button
+                        sx={{
+                            background: '#FF8C00',
+                            width: '245px',
+                            height: '53px',
+                            color: '#ffffff',
+                            boxShadow: '2px 5px 5px gray',
+                            borderRadius: '5px',
+                            fontSize: '20px'
+                        }}
+                    >ログイン</Button>
+                    <div className='text-center pt-6'>
+                        <a href='hoge' className='text-black underline'>新規登録</a>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 })
 export default Login;
